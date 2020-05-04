4dbb944bb9853978bebad80740e22c4f5fa686dd
sg1asgard <sg1asgard@outlook.com>
2020-05-04 20:32:36 +0300

    Add bookmark list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { RecipeFormComponent } from './pages/form/recipe-form.component';
 import { RecipeListComponent } from './pages/list/recipe-list.component';
 import { RecipeComponent } from './pages/view/recipe.component';
+import { BookmarkListComponent } from './components/bookmark-list/bookmark-list.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RecipeComponent } from './pages/view/recipe.component';
     RecipeFormComponent,
     RecipeListComponent,
     RecipeComponent,
+    BookmarkListComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
